24c59b347a93fdda84a967c7163777e504e9182d
Samantha Garcia <sgarcia22@ufl.edu>
2019-01-19 14:26:44 -0500

    Fixed Multiplayer Issues

--- VeggieSlam/Assets/Scripts/BallController.cs
+++ VeggieSlam/Assets/Scripts/BallController.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-
+    [SerializeField] private PhotonView photonView;
     private Rigidbody ball;
     [SerializeField]
     private float ballSpeedMultiplier = 10f;
+    [SerializeField] private Vector3 selfPosition;
+
+
     // Use this for initialization
     void Start()
     {
         ball = gameObject.GetComponent<Rigidbody>();
-        
     }
 
     // Update is called once per frame
@@ -23,7 +25,6 @@ public class BallController : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("Hitting");
         if (collision.collider.tag == "Weapon")
         {
             //ball.AddForce(new Vector3 (0f,10f,10f), ForceMode.Acceleration);
@@ -37,4 +38,13 @@ public class BallController : MonoBehaviour
             ballSpeedMultiplier += 1f;
         }
     }
+
+    private void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+            stream.SendNext(transform.position);
+        else
+            selfPosition = (Vector3)stream.ReceiveNext();
+    }
+
 }
--- VeggieSlam/Assets/Scripts/PlayerController.cs
+++ VeggieSlam/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,10 @@ public class PlayerController : MonoBehaviour {
 
     [SerializeField] private PhotonView photonView;
     [SerializeField] private Vector3 selfPosition;
+    [SerializeField] private GameObject paddle;
+    //[SerializeField] private GameObject paddle;
+
+    //private Vector3 paddlePosition;
 
     private void Start()
     {
@@ -13,16 +17,31 @@ public class PlayerController : MonoBehaviour {
         {
             gameObject.GetComponentInChildren<Camera>().enabled = true;
             selfPosition = transform.position;
+            paddle.SetActive(true);
+            //paddlePosition = paddle.transform.position;
             Debug.Log("view is mine");
+        }
+        else
+        {
+            paddle.SetActive(false);
+            gameObject.GetComponentInChildren<Camera>().enabled = false;
         }
     }
 
     void Update()
     {
+        //Writing
         if (photonView.isMine)
+        {
             selfPosition = transform.position;
+            //paddlePosition = paddle.transform.position;
+        }
+        //Reading
         else
+        {
             transform.position = selfPosition;
+            //paddle.transform.position = paddlePosition;
+        }
     }
 
     private void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
