58b92299a40742ddbecc352474803ec9e2ada977
Samantha Garcia <sgarcia22@ufl.edu>
2019-01-19 08:58:09 -0500

    Added Multiplayer - Need to fix same movements & ball

---
+++ VeggieSlam/Assets/Scripts/PhotonCommsManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon;
+
+public class PhotonCommsManager : Photon.PunBehaviour
+{
+
+    private GameObject currentPlayer;
+
+    void Start()
+    {
+        PhotonNetwork.logLevel = PhotonLogLevel.Full;
+        PhotonNetwork.ConnectUsingSettings("0.1");
+    }
+
+    public override void OnJoinedLobby()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinedRoom()
+    {
+        if (PhotonNetwork.countOfPlayersInRooms == 0)
+        // instantiate user avatar locally and spawns in remote instances
+            currentPlayer = PhotonNetwork.Instantiate("PlayerSpawn", new Vector3(0, 1.235f, 2.65f), Quaternion.identity, 0);
+        else
+            currentPlayer = PhotonNetwork.Instantiate("PlayerSpawn", new Vector3(0, 1.235f, 0f), Quaternion.identity, 0);
+    }
+
+    // This is called if there is no one playing or if all rooms are full, so create a new room
+    void OnPhotonRandomJoinFailed()
+    {
+        Debug.Log("Can't join room");
+        PhotonNetwork.CreateRoom(null);
+    }
+}
+
---
+++ VeggieSlam/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField] private PhotonView photonView;
+    [SerializeField] private Vector3 selfPosition;
+
+    private void Start()
+    {
+        if (photonView.isMine)
+        {
+            gameObject.GetComponentInChildren<Camera>().enabled = true;
+            selfPosition = transform.position;
+            Debug.Log("view is mine");
+        }
+    }
+
+    void Update()
+    {
+        if (photonView.isMine)
+            selfPosition = transform.position;
+        else
+            transform.position = selfPosition;
+    }
+
+    private void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+            stream.SendNext(transform.position);
+        else
+            selfPosition = (Vector3)stream.ReceiveNext();
+    }
+}
