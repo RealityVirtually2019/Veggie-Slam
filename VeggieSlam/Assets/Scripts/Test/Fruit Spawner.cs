eb96d3ecbc7a8086ce77370cacc11e3e86bf55d9
Samantha Garcia <sgarcia22@ufl.edu>
2019-01-19 20:03:54 -0500

    Test Scene

---
+++ VeggieSlam/Assets/Scripts/Test/Fruit Spawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitSpawner : MonoBehaviour {
+
+    [SerializeField] private Transform[] spawn;
+    [SerializeField] private GameObject [] fruit;
+    // Use this for initialization
+    void Start () {
+        InvokeRepeating("SpawnFruit", 2f, 1f);
+	}
+	
+    void SpawnFruit ()
+    {
+        int index = Random.Range(0, spawn.Length);
+        int fruitIndex = Random.Range(0, fruit.Length);
+        Instantiate(fruit[fruitIndex], spawn[index].position, Quaternion.identity, gameObject.transform);
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
